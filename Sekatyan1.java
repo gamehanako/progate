d61eca47a57cd21bad0003509b425d82002735ef
gamehanako <166238709+gamehanako@users.noreply.github.com>
2024-05-02 16:08:44 +0900

    sekatyan

---
+++ Sekatyan1.java
@@ -0,0 +1,52 @@
+class sekatyan1 {
+    public static void main(String[] args) {
+        // 変数は宣言したブロックの中でしか使えない
+//        int[][] allScore = new int[2][3];// 行（横）が２つ、列（縦）が３つ合計６個並んでいるという意味
+//        System.out.println(allScore.length);// allScoreの要素数は行が２つ並んでいるので２つ
+//        System.out.println(allScore[0].length);// allScore[0]の要素数は３つ
+//        int[][] allscore2 = {{1, 2, 3}, {4, 5, 6}};
+//        System.out.println(allscore2.length);
+//        System.out.println(allscore2[0].length);
+//        int price = Integer.parseInt(args[0]);
+//        double rate = 0.1;
+//        int discount, amount;
+//
+//        if (price >= 3000) {
+//            discount = 300;
+//        } else {
+//            discount = 0;
+//        }
+//
+//        amount = (int)((price - discount) * (1 + rate));
+//        System.out.println("discount price:" + discount);
+//        System.out.println(amount);
+//        int num = Integer.parseInt(args[0]);
+//        switch ( num ) {
+//            case 1:
+//                System.out.println(1);
+//                break;
+//            case 2:
+//                System.out.println(2);
+//                break;
+//            default:
+//                System.out.println("");
+//                break;
+//        int i = 0;
+//        while (i < 3) {
+//            System.out.println(i);
+//            i++;
+//        }
+//        // for文繰り返しの回数が決まっているときや、配列の各要素に対して同じ処理をするときに相性がいい書き方
+//        // コマンドライン引数のインデックス要素0番目をint型の変数iに代入して、10になるまで繰り返す
+//        for (int i = Integer.parseInt(args[0]); i < 10; i++) {
+//            System.out.println(i);
+//        }
+//        int radius = Integer.parseInt(args[0]);
+//        double pi = 3.141595;
+//        double area;
+//        area = radius * radius * pi;
+//        // String.format("%.2f", 変数名) は変数を四捨五入する
+//        System.out.println(String.format("%.2f", area));
+          
+    }
+}
